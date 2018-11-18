0f692d587eaa1c9f47a5162643b85e15866893ab
Brandon Herrera <brandonherrera@Brandons-MacBook-Air-3.local>
2018-11-18 18:31:22 -0500

    new catalog entry form working

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 require('./db.js');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const Catalog = mongoose.model('Catalog');
+
+//let todayCat = {};
 
 //serve static files
 const publicPath = path.resolve(__dirname, "public");
@@ -25,9 +28,43 @@ const sessionOptions = {
 };
 app.use(session(sessionOptions));
 
-app.get('/login', function(req, res) {
-	res.render('login');
+//create Food entries and add to catalog
+class Food {
+	constructor (name, time, price, cals) {
+		this.name = name;
+		this.time = time;
+		this.price = price;
+		this.cals = cals;
+	}
+}
+
+app.use("/goals", function(req, res, next) {
+	if (req.session.catalog === undefined) {
+		req.session.catalog = {};
+	}
+	next();
+});
+
+app.get('/goals', function(req, res) {
+	res.render('goals');
+});
+
+app.post('/goals', function(req, res) {
+	new Catalog({
+		date: req.body.date,
+		calGoal: req.body.cal,
+		monGoal: req.body.mon
+	}).save( (err, cat) => {
+		//console.log(err);
+		req.session.catalog = cat;
+		res.redirect('/home')
+	});
+});
+
+app.get('/home', function(req, res) {
+	res.render('home', {"todayCat": req.session.catalog});
 });
 
 app.listen(3000);
+
 console.log("Connected to server");
--- db.js
+++ db.js
@@ -3,11 +3,41 @@ const mongoose = require('mongoose');
 //schema for new User
 const User = new mongoose.Schema({
 	username: String,
-	hash: //hash from bcrypt,
+	//hash: //hash from bcrypt,
 	catalog: Object //dictionary like object to store dates mapped to lists of food
 
 });
+//schema for new catalog for a specific date
+const Catalog = new mongoose.Schema({
+	username: String,
+	date: String,
+	calGoal: Number,
+	monGoal: Number, 
+	foods: Array
+});
 
 mongoose.model("User", User);
+mongoose.model("Catalog", Catalog);
 
-mongoose.connect('mongodb://localhost/db', {useNewUrlParser: true});
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+	 // if we're in PRODUCTION mode, then read the configration from a file
+	 // use blocking file io to do this...
+	 const fs = require('fs');
+	 const path = require('path');
+	 const fn = path.join(__dirname, 'config.json');
+	 const data = fs.readFileSync(fn);
+
+	 // our configuration file will be in json, so parse it and set the
+	 // conenction string appropriately!
+	 const conf = JSON.parse(data);
+	 dbconf = conf.dbconf;
+} else {
+	 // if we're not in PRODUCTION mode, then use
+	 dbconf = 'mongodb://localhost/bh1505';
+}
+
+mongoose.connect(dbconf, {useNewUrlParser: true});
+
+
