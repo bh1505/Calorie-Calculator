45726a9049dd7b5b061a024858743a5c820a1775
Brandon Herrera <brandonherrera@Brandons-MacBook-Air-3.local>
2018-11-19 16:07:35 -0500

    user authentication working

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const session = require('express-session');
+//const jquery = require('jquery');
 
 require('./db.js');
+require('./auth');
+const passport = require('passport');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Catalog = mongoose.model('Catalog');
-const Food = mongoose.model('Food');
+//const Food = mongoose.model('Food');
 
-//let todayCat = {};
+//class for each new Food entry
+class Food {
+	constructor(name, time, price, cals) {
+		this.name = name;
+		this.time = time;
+		this.price = price;
+		this.cals = cals;
+	}
+}
 
 //serve static files
 const publicPath = path.resolve(__dirname, "public");
@@ -29,6 +40,8 @@ const sessionOptions = {
 };
 app.use(session(sessionOptions));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(function(req, res, next) {
 	if (req.session.catalog === undefined) {
@@ -37,8 +50,73 @@ app.use(function(req, res, next) {
 	next();
 });
 
+app.use(function(req, res, next){
+	res.locals.user = req.user;
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.redirect('/login');
+})
+
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+
+app.post('/login', function(req, res) {
+	passport.authenticate('local', function(err,user) {
+    if(user) {
+      req.logIn(user, function(err) {
+        res.redirect('/goals');
+      });
+    } else {
+      	res.render('login', {message:'Your login or password is incorrect.'});
+    }
+  })(req, res);
+});
+
+app.get('/register', function(req, res) {
+	res.render('register');
+});
+
+app.post('/register', function(req, res) {
+	User.find({username: req.body.username}, (err, result) => {
+		if (result.length > 0) {
+			console.log("USERNAME ALREADY EXISTS");
+			res.render('register', {message: "USERNAME ALREADY EXISTS"});
+		} 
+		else if (req.body.username.length < 8 || req.body.password.length < 8) {
+			res.render('register', {message: "Username or Password to Short"})
+		} else {
+			User.register(new User({username:req.body.username}), 
+		      req.body.password, function(err, user){
+		    	if (err) {
+		      		res.render('register',{message:'Your registration information is not valid'});
+		    	} else {
+		      		passport.authenticate('local')(req, res, function() {
+		        		res.redirect('/goals');
+		      		});
+		  	  	}
+		  	});
+		}
+	});
+
+	
+});
+
 app.get('/goals', function(req, res) {
-	res.render('goals');
+	//check if currently working on catalog
+	//console.log(jquery);
+	const obj = req.user.catalogs[req.user.catalogs.length-1];
+	if (obj === undefined) {
+		res.render('goals');
+	} else if (obj.completed) {
+		res.render('goals');
+	} else {
+		req.session.catalog = obj;
+		res.redirect('/home');
+	}
+	
 });
 
 app.post('/goals', function(req, res) {
@@ -47,10 +125,13 @@ app.post('/goals', function(req, res) {
 		calGoal: req.body.cal,
 		monGoal: req.body.mon,
 		curCal: 0,
-		curMon: 0
+		curMon: 0,
+		completed: false
 	}).save( (err, cat) => {
 		//console.log(err);
 		req.session.catalog = cat;
+		req.user.catalogs.push(cat);
+		req.user.save();
 		res.redirect('/home')
 	});
 });
@@ -65,24 +146,34 @@ app.get('/add', function(req, res) {
 });
 
 app.post('/add', function(req, res) {
-	new Food({
+	/*new Food({
 		name: req.body.name,
 		time: req.body.time,
 		price: req.body.price,
 		cals: req.body.cals
-	}).save( (err, food) => {
-		req.session.catalog.foods.push(food);
+	}).save( (err, food) => {*/
+		//update catalog
+		//replace with req.user
+		const f = new Food (req.body.name, req.body.time, req.body.price, req.body.cals);
+		req.session.catalog.foods.push(f);
 		req.session.catalog.curCal += Number(req.body.cals);
 		req.session.catalog.curMon += Number(req.body.price);
-		//update catalog
-		Catalog.findOne({date: req.session.catalog.date}, (err, cat) => {
-			cat.foods.push(food);
+		//update user's catalog
+		req.user.catalogs[req.user.catalogs.length-1].foods.push(f);
+		req.user.catalogs[req.user.catalogs.length-1].curMon +=  Number(f.price);
+		req.user.catalogs[req.user.catalogs.length-1].curCal +=  Number(f.cals);
+		req.user.save();
+
+		/*Catalog.findOne({date: req.session.catalog.date}, (err, cat) => {
+			cat.foods.push(f);
+			cat.curMon += Number(f.price);
+			cat.curCal += Number(f.cals);
 			cat.save( function(err, c) {
 				console.log(c);
 			});
-		});
+		});*/
 		res.redirect('/home');
-	});
+	//});
 });
 
 app.listen(process.env.PORT || 3000);
--- db.js
+++ db.js
@@ -1,19 +1,5 @@
 const mongoose = require('mongoose');
-
-//schema for new User
-const User = new mongoose.Schema({
-	username: String,
-	//hash: //hash from bcrypt,
-	catalogs: Object //dictionary like object to store dates mapped to lists of food
-
-});
-
-const Food = new mongoose.Schema({
-	name: String,
-	time: String,
-	price: Number,
-	cals: Number
-});
+const passportLocalMongoose = require('passport-local-mongoose');
 
 //schema for new catalog for a specific date
 const Catalog = new mongoose.Schema({
@@ -23,13 +9,30 @@ const Catalog = new mongoose.Schema({
 	monGoal: Number,
 	curCal: Number,
 	curMon: Number,
-	foods: [Food]
+	foods: Array,
+	completed: Boolean //determines whether user's latest catalog is complete for the day
 });
 
+//schema for new User
+const User = new mongoose.Schema({
+	//username: String,
+	//hash: //hash from bcrypt,
+	catalogs: [Catalog] //dictionary like object to store dates mapped to lists of food
+
+});
+
+/*const Food = new mongoose.Schema({
+	name: String,
+	time: String,
+	price: Number,
+	cals: Number
+});*/
+
+User.plugin(passportLocalMongoose);
 
 mongoose.model("User", User);
 mongoose.model("Catalog", Catalog);
-mongoose.model("Food", Food);
+//mongoose.model("Food", Food);
 
 // is the environment variable, NODE_ENV, set to PRODUCTION? 
 let dbconf;
@@ -49,6 +52,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
 	 // if we're not in PRODUCTION mode, then use
 	 dbconf = 'mongodb://localhost/bh1505';
 }
+
 
 mongoose.connect(dbconf, {useNewUrlParser: true});
 
